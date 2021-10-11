7dc1842657fcba4698f61f005719a3bd69908827
DESKTOP-DVPF2K2\U <matveychukivan.r@gmail.com>
2021-10-11 19:14:07 +0300

    fix lesson 2

--- src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
+++ src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
@@ -6,7 +6,6 @@ public class NonNegativeSequence {
     последовательности 90 85 80 75 70 65 60 ….
      */
     public static void main(String[] args) {
-
         for (int i = 90; i >= 0 ; i -= 5) {
             System.out.println(i);
         }
