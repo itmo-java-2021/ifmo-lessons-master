4e65236d4fbe040f7f116908ac2ac07d6d86813b
DESKTOP-DVPF2K2\U <matveychukivan.r@gmail.com>
2021-10-11 19:10:13 +0300

    fix lesson 2

--- src/main/java/com/ifmo/lesson2/Dividers.java
+++ src/main/java/com/ifmo/lesson2/Dividers.java
@@ -9,9 +9,7 @@ public class Dividers {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
-
         for (int i = 1; i <= n; i++) {
             if (n % i == 0){
                 System.out.println(i);
