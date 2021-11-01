c5f30c5e416e48d37389f5a3c2495915e673008c
Ivan.Matveychuk <Ivan.Matveychuk@sptz.kzgroup.ru>
2021-11-01 13:36:00 +0300

    lesson 11 Tasks1

--- src/main/java/com/ifmo/lesson11/Tasks1.java
+++ src/main/java/com/ifmo/lesson11/Tasks1.java
@@ -4,8 +4,8 @@ import com.ifmo.lesson11.inner.Message;
 import com.ifmo.lesson11.inner.MessageGenerator;
 import com.ifmo.lesson11.inner.MessagePriority;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
     Реализуйте все методы.
@@ -31,21 +31,70 @@ public class Tasks1 {
         // Сосчитайте количество сообщений для каждого приоритета.
         // Ответ необходимо вывести в консоль.
 
-        // TODO implement
+        int[] vs = new int[4];
+
+        for (Message message: messages){
+            switch (message.getPriority()){
+                case LOW -> {
+                    vs[0]++;
+                }
+                case MEDIUM -> {
+                    vs[1]++;
+                }
+                case HIGH -> {
+                    vs[2]++;
+                }
+                case URGENT -> {
+                    vs[3]++;
+                }
+            }
+        }
+        System.out.printf("""
+                Count:
+                    -LOW %d
+                    -MEDIUM %d
+                    -HIGH %d
+                    -URGENT %d
+                """,
+                vs[0],
+                vs[1],
+                vs[2],
+                vs[3]);
     }
 
     private static void countCountEachCode(List<Message> messages) {
         // Сосчитайте количество сообщений для каждого кода сообщения.
         // Ответ необходимо вывести в консоль.
 
-        // TODO implement
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Message message: messages){
+            int code = message.getCode();
+            if (map.containsKey(code)){
+                map.replace(code, map.get(code) + 1);
+            } else {
+                map.put(code, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.printf("code %d: %d \n", entry.getKey(), entry.getValue());
+        }
     }
 
     private static void countUniqueMessages(List<Message> messages) {
         // Сосчитайте количество уникальных сообщений.
         // Ответ необходимо вывести в консоль.
 
-        // TODO implement
+        Map<Message, Integer> map = new HashMap<>();
+        for (Message message: messages){
+            if (map.containsKey(message)){
+                map.replace(message, map.get(message) + 1);
+            } else {
+                map.put(message, 1);
+            }
+        }
+        for (Map.Entry<Message, Integer> entry : map.entrySet()) {
+            System.out.printf("message %s: %d \n", entry.getKey().toString(), entry.getValue());
+        }
     }
 
     private static List<Message> genuineMessagesInOriginalOrder(List<Message> messages) {
@@ -56,16 +105,35 @@ public class Tasks1 {
         // [{URGENT, 4}, {HIGH, 9}, {LOW, 3}].
         // Т.е. остались только уникальные значения, и порядок их поступления сохранен.
 
-        // TODO implement
+        Map<Message, Integer> map = new LinkedHashMap<>();
+        for (Message message: messages){
+            if (map.containsKey(message)){
+                map.replace(message, map.get(message) + 1);
+            } else {
+                map.put(message, 1);
+            }
+        }
+        List<Message> list = new ArrayList<>();
+        for (Map.Entry<Message, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1){
+                list.add(entry.getKey());
+            }
+        }
 
-        return messages;
+        return list;
     }
 
     private static void removeEach(Collection<Message> messages, MessagePriority priority) {
         // Удалить из коллекции каждое сообщение с заданным приоритетом.
         System.out.printf("Before remove each: %s, %s\n", priority, messages);
 
-        // TODO implement
+        Iterator iterator = messages.iterator();
+        while (iterator.hasNext()){
+            Message message = (Message) iterator.next();
+            if (message.getPriority().equals(priority)){
+                iterator.remove();
+            }
+        }
 
         System.out.printf("After remove each: %s, %s\n", priority, messages);
     }
@@ -74,7 +142,13 @@ public class Tasks1 {
         // Удалить из коллекции все сообщения, кроме тех, которые имеют заданный приоритет.
         System.out.printf("Before remove other: %s, %s\n", priority, messages);
 
-        // TODO implement
+        Iterator iterator = messages.iterator();
+        while (iterator.hasNext()){
+            Message message = (Message) iterator.next();
+            if (!message.getPriority().equals(priority)){
+                iterator.remove();
+            }
+        }
 
         System.out.printf("After remove other: %s, %s\n", priority, messages);
     }
