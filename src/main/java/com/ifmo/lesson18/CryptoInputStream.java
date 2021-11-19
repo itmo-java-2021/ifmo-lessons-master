59dfa219707909e45daa1f28397b0f9532fedde7
Ivan.Matveychuk <Ivan.Matveychuk@sptz.kzgroup.ru>
2021-11-19 10:19:43 +0300

    lesson 18 fix

--- src/main/java/com/ifmo/lesson18/CryptoInputStream.java
+++ src/main/java/com/ifmo/lesson18/CryptoInputStream.java
@@ -1,5 +1,7 @@
 package com.ifmo.lesson18;
 
+import com.ifmo.lesson3.EncryptArray;
+
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,7 +22,6 @@ public class CryptoInputStream extends FilterInputStream {
      */
 
     private byte[] key;
-    private int nextKey;
 
     public CryptoInputStream(InputStream in, byte[] key) {
         super(in);
@@ -28,13 +29,8 @@ public class CryptoInputStream extends FilterInputStream {
     }
 
     @Override
-    public int read() throws IOException {
-        int b = super.read() ^ key[nextKey];
-        if (key.length - 1 == nextKey){
-            nextKey = 0;
-        } else {
-            nextKey++;
-        }
-        return b;
+    public int read(byte[] b, int off, int len) throws IOException {
+        EncryptArray.encrypt(b, key);
+        return super.read(b, off, len);
     }
 }
